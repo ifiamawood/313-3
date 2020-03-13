fb4990375f269e6be4ef9f28c88068962354cfea
ifiamawood <821371350@qq.com>
2020-03-13 14:51:35 +0800

    1

--- 313 3/313 3.cpp
+++ 313 3/313 3.cpp
@@ -2,10 +2,39 @@
 //
 
 #include "stdafx.h"
+#include<iostream>
+#include<opencv.hpp>
 
+using namespace cv;
+using std::string;
 
+string window_name = "binaryMat";
+
+void threshod_Mat(int th, void*data)
+{
+	Mat src = *(Mat*)(data);
+	Mat dst;
+	threshold(src, dst, th, 255, 0);
+	imshow(window_name, dst);
+}
 int main()
 {
-    return 0;
+	Mat srcMat, gryMat;
+	int low = 30, max = 255;
+	srcMat = imread("D:\\1.jpg");
+	if (!srcMat.data) {
+		std::cout << "Í¼ÏñÔØÈëÊ§°Ü" << std::endl;
+		return 0;
+	}
+	cvtColor(srcMat, gryMat, CV_BGR2GRAY);
+	imshow(window_name, gryMat);
+	createTrackbar("threshold",
+		window_name,
+		&low,
+		max,
+		threshod_Mat,
+		&gryMat);
+	waitKey(0);
+	return 0;
 }
 
